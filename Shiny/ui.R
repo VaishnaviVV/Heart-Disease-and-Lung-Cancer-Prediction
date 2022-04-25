e03149975bc661db16b6d460769a854d60661cf6
Shalini180 <66202473+Shalini180@users.noreply.github.com>
2022-04-25 08:16:16 +0530

    Add files via upload

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -4,18 +4,122 @@ library(dplyr)
 library(shinythemes)
 library(data.table)
 library(ggplot2)
+library(shinydashboard)
+library(knitr)
+library(bslib)
 shinyUI(
   
   dashboardPage(
-    dashboardHeader(title = "OLYMPIC GAMES"),
+    dashboardHeader(title = "Lung Cancer DashBoard"),
     
     dashboardSidebar(sidebarMenu(
+      menuItem("LevelOfRisk",tabName="LevelOfRisk",icon=icon("temperature-high")),
+      menuItem("Distribution_Of_Anomalies",tabName="Distribution_Of_Anomalies",icon=icon("money")),
       menuItem("prediction",tabName="prediction",icon=icon("snowflake"))
       
     )),
     dashboardBody(  
       tabItems(
-        
+        tabItem("Distribution_Of_Anomalies",
+                fluidPage(
+                  fluidRow(
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot"), 
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    ),
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot1"), 
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    )
+                  ),
+                  fluidRow(
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot2"), 
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    ),
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot3"), 
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    )
+                  ),
+                  fluidRow(
+                    column(# width should be between 1 and 12
+                      width=12,
+                      box(plotlyOutput("plot20"), 
+                          
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    )
+                    
+                  )
+                )
+        ),
+        tabItem("LevelOfRisk",
+                fluidPage(
+                  fluidRow(
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot4"), 
+                          
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    ),
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot5"), 
+                          
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    )
+                    
+                  ),
+                  fluidRow(
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot6"), 
+                          
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    ),
+                    column(# width should be between 1 and 12
+                      width=6,
+                      box(plotlyOutput("plot7"), 
+                          
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    )
+                    
+                  ),
+                  fluidRow(
+                    column(# width should be between 1 and 12
+                      width=12,
+                      box(plotlyOutput("plot21"), 
+                          
+                          # For column based layouts, we can set box width to NULL
+                          # This overrides the default value
+                          width=NULL) 
+                    )
+                    
+                  )
+                )
+        ),
         tabItem("prediction",
                 fluidPage(
                   
@@ -32,7 +136,7 @@ shinyUI(
                     
                     width=8,
                     box(##textOutput("textoutput1"),
-                      height = "180vh", title = "Probability of getting Lung Cancer",theme = shinytheme("cerulean"),
+                      height = "220vh", title = "Probability of getting Lung Cancer",theme = shinytheme("cerulean"),
                       selectInput("GENDER", "GENDER", choices = c("M","F")),
                       numericInput("AGE", "AGE",  10, min = 1, max = 100),
                       selectInput("SMOKING", "SMOKING", choices = c(1,2)),
@@ -63,8 +167,3 @@ shinyUI(
   )
   
 )
-
-
-
-
-
